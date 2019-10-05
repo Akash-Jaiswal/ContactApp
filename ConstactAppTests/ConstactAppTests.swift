17c9ee175125be2a1105f13743f84df2abcd6bdb
Akash-Jaiswal <akash.jaiswal@conduent.com>
2019-10-05 14:40:59 +0530

    Initial Commit

---
+++ ConstactAppTests/ConstactAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConstactAppTests.swift
+//  ConstactAppTests
+//
+//  Created by Jaiswal, Akash on 05/10/19.
+//  Copyright © 2019 Jaiswal, Akash. All rights reserved.
+//
+
+import XCTest
+@testable import ConstactApp
+
+class ConstactAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
