718fdf37c693c3cbded414911e27a1d926b2ee3b
Akash-Jaiswal <akash.jaiswal@conduent.com>
2019-10-06 09:53:41 +0530

    Added contact Model

--- ConstactApp/Presentation/ContactModel.swift
+++ ConstactApp/Presentation/ContactModel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+public enum Account {
+    public struct Request: Codable {
+        public var username: String?
+        public init() {
+        }
+        public init(username: String?) {
+            self.username = username
+        }
+    }
+    public struct Response: Codable {
+        public var  id: Int?
+        public var  firstName, lastName: String?
+        public var  profilePic: String?
+        public var  favorite: Bool?
+        public var  url: String?
+        public init() {
+        }
+        public init(id: Int?,firstName: String?, lastName: String?, profilePic: String?, favorite: Bool?, url: String?) {
+            self.id = id
+            self.firstName = firstName
+            self.lastName = lastName
+            self.profilePic = profilePic
+            self.favorite = favorite
+            self.url = url
+        }
+    }
+}
